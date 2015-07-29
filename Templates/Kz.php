af9dfb8db70664ee3f3715382b6d3e8ff714ecd3
Alexey Ashurok <me@aotd.ru>
2015-07-29 15:10:03 +0600

    Improve .kz whois parser

--- Templates/Kz.php
+++ Templates/Kz.php
@@ -47,7 +47,9 @@ class Kz extends Regex
             1 => '/Organization Using Domain Name(.*?)(?=Administrative Contact\/Agent)/is', 
             2 => '/Administrative Contact\/Agent(.*?)(?=Nameserver in listed order)/is', 
             3 => '/Nameserver in listed order(.*?)(?=Domain created)/is', 
-            4 => '/Domain created(.*?)$/is');
+            4 => '/Domain created(.*?)$/is',
+            5 => '/Registar (.*?)$/is'
+    );
 
     /**
 	 * Items for each block
@@ -57,25 +59,34 @@ class Kz extends Regex
 	 */
     protected $blockItems = array(
             1 => array(
-                    '/^Name(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:owner:name', 
-                    '/^Organization Name(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:owner:organization', 
-                    '/^Street Address(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:owner:address', 
-                    '/^City(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:owner:city', 
-                    '/^State(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:owner:state', 
-                    '/^Postal Code(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:owner:zipcode', 
-                    '/^City(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:owner:city'), 
+                '/^Name(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:owner:name',
+                '/^Organization Name(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:owner:organization',
+                '/^Street Address(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:owner:address',
+                '/^City(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:owner:city',
+                '/^State(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:owner:state',
+                '/^Postal Code(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:owner:zipcode',
+                '/^City(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:owner:city'
+            ),
             2 => array(
-                    '/^NIC Handle(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:handle', 
-                    '/^Name(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:admin:name', 
-                    '/^Phone Number(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:phone', 
-                    '/^Fax Number(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:fax', 
-                    '/^Email Address(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:email'), 
+                '/^NIC Handle(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:handle',
+                '/^Name(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.*)$/im' => 'contacts:admin:name',
+                '/^Phone Number(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:phone',
+                '/^Fax Number(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:fax',
+                '/^Email Address(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'contacts:admin:email'
+            ),
             3 => array(
-                    '/^(Primary|Secondary) server(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'nameserver', 
-                    '/^(Primary|Secondary) ip address(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'ips'), 
-            4 => array('/^Last modified(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'changed', 
-                    '/^Domain created(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'created', 
-                    '/^Domain status(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'status'));
+                '/^(Primary|Secondary) server(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'nameserver',
+                '/^(Primary|Secondary) ip address(?>[\.]*)(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'ips'
+            ),
+            4 => array(
+                '/^Last modified(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'changed',
+                '/^Domain created(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'created',
+                '/^Domain status(?>[\x20\t]*):(?>[\x20\t]*)(.+)$/im' => 'status'
+            ),
+            5 => array(
+                '/Current Registar:(?>[\x20\t]*)(.+)$/im' => 'registrar:name',
+            ),
+    );
 
     /**
      * RegEx to check availability of the domain name
@@ -84,4 +95,19 @@ class Kz extends Regex
      * @access protected
      */
     protected $available = '/Nothing found for this query/i';
+
+    public function postProcess(&$WhoisParser)
+    {
+        $ResultSet = $WhoisParser->getResult();
+
+        // check if there was another whois server
+        if (isset($ResultSet->registrar->name)) {
+            $Config = $WhoisParser->getConfig();
+            $registrar = strtolower(trim($ResultSet->registrar->name));
+            $newConfig = $Config->get($registrar);
+
+            $Config->setCurrent($newConfig);
+            $WhoisParser->call();
+        }
+    }
 }
